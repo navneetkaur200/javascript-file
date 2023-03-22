f6111183d1aceb289facde9a9c900cfcdadebde8
unknown <navneetkaur1531988@gmail.com>
2023-03-22 09:51:33 +0100

    class.js added

---
+++ class.js
@@ -0,0 +1,44 @@
+// class user{
+//     constructor(x,y,z)
+//     {
+//    this.firstname=x,//To access a property of an object from within a method of the same object, you need to use the this keyword.  
+//     this.lastname=y,
+//     this.age=z,
+//     this.fullname=()=>{console.log(this.firstname+this.lastname)}
+//     }
+// }
+// c=new user("navneet","kaur",23)
+// console.log(c)
+// console.log(c.fullname())
+  
+
+// class student{
+//     constructor(a,b,c,d)
+//     {
+//         this.Name=a,
+//         this.Id=b,
+//         this.Course=c,
+//         this.Address=d,
+//        this.details=()=>{console.log(this.Name+" "+this.Id+" "+this.Course) }
+     
+//    }
+// }  
+// Demo=new student("navneet kaur",21072111,"Javascript","khanna")
+
+// console.log(Demo)
+
+// console.log(Demo.details())
+
+
+// class stu{
+//     constructor()
+//     {
+//         this.student_name="navi",
+//         this.department_name="computer science",
+//         this.uni_name="world university fgs"}
+// }
+//  dd=new stu()
+// console.log(dd)
+
+
+//
