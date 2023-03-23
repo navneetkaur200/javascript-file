ae49f5c77b9be38405fff8f76106fd459a630242
unknown <navneetkaur1531988@gmail.com>
2023-03-23 08:13:15 +0100

    add async file

---
+++ async.js
@@ -0,0 +1,16 @@
+name1="navneet kaur"
+setTimeout(()=>{console.log(`the name:${name1};`)},1000);
+function callname(name1)
+{
+    console.log(`name call in function:${name1}`)
+}
+callname(name1)
+arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+for(i=0;i<arr.length;i++)
+{
+    for(j=0;j<arr.length;j++)
+    {
+        console.log(arr[j])
+    }
+    console.log(arr[i])
+}
