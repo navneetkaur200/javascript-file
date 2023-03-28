728365e1f26bd0ad35f3c4ab636a25fed1c2505c
unknown <navneetkaur1531988@gmail.com>
2023-03-28 10:51:28 +0100

    regular expresion file add

--- regularexpression.js
+++ regularexpression.js
@@ -30,19 +30,106 @@
 
 
 
-     re =new RegExp (/\S+@\S+\.\S+/g)
-    result = re.test('navneet @gmail.com');
-    if (result) {
-        console.log('The email is valid.');
-    }
-    else {
-        console.log('enter valid email')
-    }
+    //  re =new RegExp (/\S+@\S+\.\S+/g)
+    // result = re.test('navneet @gmail.com');
+    // if (result) {
+    //     console.log('The email is valid.');
+    // }
+    // else {
+    //     console.log('enter valid email')
+    // }
   
-    re = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/g;
-    result = re.test(9877913515);
-    if (result) {
-        console.log('The number is valid.');
-    }
-    else {
-      console.log('number is not valid')}
+    // re = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/g;
+    // result = re.test(9877913515);
+    // if (result) {
+    //     console.log('The number is valid.');
+    // }
+    // else {
+    //   console.log('number is not valid')}
+
+
+// program to validate the phone number
+// function validnumber(num){
+//     regexp = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/g;
+//     result = num.match(regexp);
+
+//     if (result) {
+//         console.log('The number is valid.');
+//     }
+//     else {
+     
+//         console.log(`the ${num} format is not valid. The valid format is apply xxx-xxx-xxxx.`)
+//      }
+// }
+// validnumber("578-482-5895")
+
+// //the program is validate the email address.
+// function validemail(mail){
+//     re=/\S+@\S+\.\S+/g;
+//     result1=mail.match(re);
+//     if(result1)
+//     {
+//         console.log(`The ${mail} is valid`)
+//     }
+//     else{
+//         console.log(`The ${mail} is not valid. Please Enter the valid ${mail} Format.`)
+//     }
+// // }
+//     validemail("gk4636963  @gmail.com")
+
+// class user{
+// constructor(a,b)
+// {
+//      this.email=a;
+//      this.password=b;
+    
+// }
+//   static person=(a,b)=>{
+//   var mail=/\S+@\S+\.\S+/g; 
+//   var pass= /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/g;
+//  if(!mail.test(a))
+//  {
+//     return `not valid email`
+//  } 
+//  if(!pass.test(b))
+//  {
+//     return `not valid password`
+//  }
+//  return new user(a,b)
+//     } 
+// }
+//  newuser=user.person("navneet153@gmail.com",'12345678911')
+// console.log(newuser)
+
+// class user{
+//  constructor(email,password)
+//  {
+// this.email=email,
+// this.password=password;
+// }
+// validateemail(email)
+// {
+//    email=/\S+@\S+\.\S+/g;
+  
+//    if( this.email.match(email))
+//    {
+//        console.log(`The email is valid`)
+//    }
+//    else{
+//        console.log(`The emailis not valid. Please Enter the valid email Format.`)
+//    }
+// }
+// validatepassword(password){
+//    password=/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/g;
+//   if( this.password.match(password))
+//    {
+//        console.log(`The password is valid`)
+//    }
+//    else{
+//        console.log(`The password is  not valid. Please Enter the valid password.`)
+//    }
+// }
+// }
+// person=new user("navneet@gmail.com",'1234567899')
+// person.validateemail()
+// person.validatepassword()
